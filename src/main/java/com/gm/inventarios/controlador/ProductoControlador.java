7b4d261a62ae36cd5520ab62d6d592ab3e9ee916
rod773 <104315743+rod773@users.noreply.github.com>
2023-08-16 11:13:15 +0200

    obtener producto por id

--- src/main/java/com/gm/inventarios/controlador/ProductoControlador.java
+++ src/main/java/com/gm/inventarios/controlador/ProductoControlador.java
@@ -1,11 +1,13 @@
 package com.gm.inventarios.controlador;
 
 
+import com.gm.inventarios.excepcion.RecursoNoEncontradoExcepcion;
 import com.gm.inventarios.modelo.Producto;
 import com.gm.inventarios.servicio.ProductoServicio;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -37,5 +39,18 @@ public class ProductoControlador {
         logger.info("Producto a Agregar : "+producto);
 
         return this.productoServicio.guardarProducto(producto);
+    }
+
+
+    @GetMapping("productos/{id}")
+    public ResponseEntity<Producto> obtenerProductoPorId(@PathVariable int id)
+    {
+       Producto producto = productoServicio.buscarProductoPorId(id);
+
+if(producto != null)
+       return ResponseEntity.ok(producto);
+
+else
+    throw new RecursoNoEncontradoExcepcion("no se encontró el id : "+id);
     }
 }
